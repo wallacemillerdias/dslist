f6e828e18399955dc9dd990fb5d405686dcb60df
wallacemiller <wallacemillerdias@gmail.com>
2024-11-15 18:53:07 -0300

    :sparkles: Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -11,25 +11,33 @@ public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String title;
 
     @Column(name="game_year")
     private Integer year;
+
     private String genre;
     private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -73,6 +81,14 @@ public class Game {
 
     public void setPlatforms(String platforms) {
         this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
